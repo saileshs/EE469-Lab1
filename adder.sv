923a66f132d5277113934dabaf4a77a977918dc4
SAILESH-PC\Sailesh <sailesh@uw.edu>
2016-10-19 17:10:28 -0700

    fixed adder/subtractor

--- adder.sv
+++ adder.sv
@@ -1,39 +1,46 @@
-module adder64 (carryOut, sum, a, b);
-	output logic [63:0] sum;
-	output logic carryOut;
-	input logic [63:0] a, b;
-	
-	addSub64 add (.carryOut, .sum, .a, .b, .carryIn(1'b0));
-	
-endmodule
+//module adder64 (carryOut, sum, a, b);
+//	output logic [63:0] sum;
+//	output logic carryOut;
+//	input logic [63:0] a, b;
+//	
+//	addSub64 add (.carryOut, .sum, .a, .b, .carryIn(1'b0));
+//	
+//endmodule
+//
+//module subtractor64 (carryOut, diff, a, b);
+//	output logic [63:0] diff;
+//	output logic carryOut;
+//	input logic [63:0] a, b;
+//	
+//	logic [63:0] notB;
+//	
+//	not64	 invert (.out(notB), .in(b));
+//	
+//	addSub64 subtract (.carryOut, .sum(diff), .a, .b(notB), .carryIn(1'b1));
+//	
+//endmodule
 
-module subtractor64 (carryOut, diff, a, b);
-	output logic [63:0] diff;
-	output logic carryOut;
-	input logic [63:0] a, b;
-	
-	logic [63:0] notB;
-	
-	not64	 invert (.out(notB), .in(b));
-	
-	addSub64 subtract (.carryOut, .sum(diff), .a, .b(notB), .carryIn(1'b1));
-	
-endmodule
-
-module addSub64 (carryOut, sum, a, b, carryIn);
-	output logic [63:0] sum;
-	output logic carryOut;
+module addSub64 (carryOut, result, overflow, a, b, carryIn);
+	output logic [63:0] result;
+	output logic carryOut, overflow;
 	input logic [63:0] a, b;
 	input logic carryIn;
 	
 	wire [63:0] tempCarryOut;
-
-	addSub add0 (.carryOut(tempCarryOut[0]), .sum(sum[0]), .a(a[0]), .b(b[0]), .carryIn);
 	
+	logic [63:0] secondOp;
+	logic [63:0] notB;
+	
+	not64	 invert (.out(notB), .in(b));
+	mux_2to1 m0 (.out(secondOp), .control(carryIn), .in({notB, b}));
+
+	addSub add0 (.carryOut(tempCarryOut[0]), .sum(result[0]), .a(a[0]), .b(secondOp[0]), .carryIn);
+
+		
 	genvar i;
 	generate
 		for(i = 1; i < 64; i++) begin : eachAdder
-			adder addSub (.carryOut(tempCarryOut[i]), .sum(sum[i]), .a(a[i]), .b(b[i]), .carryIn(tempCarryOut[i - 1]));
+			adder addSub (.carryOut(tempCarryOut[i]), .sum(result[i]), .a(a[i]), .b(secondOp[i]), .carryIn(tempCarryOut[i - 1]));
 		end
 	endgenerate
 	assign carryOut = tempCarryOut[63];
--- alu.sv
+++ alu.sv
@@ -6,12 +6,13 @@ module alu (A, B, cntrl, result, negative, zero, overflow, carry_out);
 	
 	logic [63:0] add_out, sub_out, and_out, or_out, nor_out, less_than_out;
 	
-	adder64 adder (.carryOut(carry_out), .sum(add_out), .a(A), .b(B));
-	subtractor64 subtract (.carryOut(carry_out), .diff(sub_out), .a(A), .b(B));
+	
+	addSub64 addsub (.carryOut(carry_out), .result, .a(A), .b(B), .carryIn(cntrl[0]));
 	and64 ander (.out(and_out), .a(A), .b(B));
 	or64 orer (.out(or_out), .a(A), .b(B));
 	nor64 norer (.out(nor_out), .a(A), .b(B));
 	
 	mux_8to1 m (.out(result), .control(cntrl), .in({64'bx, 64'bx, nor_out, less_than_out, sub_out, add_out, or_out, and_out}));
+	
 	
 endmodule
